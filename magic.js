668e933794d2805e66651a6565aaa40f344502a6
SilverNeko21 <larong2122@gmail.com>
2017-11-26 14:10:15 -1000

    done

--- magic.js
+++ magic.js
@@ -1,4 +1,4 @@
-var blue = 50000;
+var blue = 1999;
 var green = 5002;
 var red = 2999;
 var output ;
@@ -7,16 +7,16 @@ function invasion(x, y, z) {
   if ((x + y + z) == 10000) {
     
     if (y > 5000) {
-      output = "MOTHER OF GOD, not the green ones"
+      output = "MOTHER OF GOD, not the green ones";
     } else if ((y + z) > 8000) {
-      output = "DOOOOOM"
+      output = "DOOOOOM";
     } else if ((z + x) < 3000) {
-      output = "We will not go quietly into the night. We will not vanish without a fight. TODAY WE CELEBRATE OUR INDEPENDANCE DAY!"
+      output = "We will not go quietly into the night. We will not vanish without a fight. TODAY WE CELEBRATE OUR INDEPENDANCE DAY!";
     } else {
-      output = "how did u manage to mess this up!?!?!?"
+      output = "how did u manage to mess this up!?!?!?";
     }
   } else {
-    output = "Too many aliens boi."
+    output = "Too many aliens boi.";
   }
 }
 
